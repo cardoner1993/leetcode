2fd0bec70c15796f0383e9278187833d0da5c521
David Cardoner <cardoner1993@gmail.com>
2022-04-15 16:11:21 +0200

    Create linked-list-cycle.scala

---
+++ linked-list-cycle.scala
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode(var _x: Int = 0) {
+ *   var next: ListNode = null
+ *   var x: Int = _x
+ * }
+ */
+
+object Solution {
+    def hasCycle(head: ListNode): Boolean = {
+        var fast = head;
+        var slow = head;
+ 
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+ 
+            if(slow == fast)
+                return true;
+        }
+ 
+        return false;
+    }
+}
