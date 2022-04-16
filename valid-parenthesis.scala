6bc80ef049a1d785fe4eac4506167b976fa0d886
David Cardoner <cardoner1993@gmail.com>
2022-04-16 02:23:16 +0200

    Update valid-parenthesis.scala

--- valid-parenthesis.scala
+++ valid-parenthesis.scala
@@ -1,3 +1,4 @@
+// Check if left parentesis is matched by its right part or return false. If top element and not empty pop the first element of the stack.
 object Solution {
     def isValid(s: String): Boolean = {
         var stackobj = scala.collection.mutable.Stack[Char]()
