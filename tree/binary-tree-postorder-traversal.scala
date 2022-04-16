eb814698debb3f6aafccc71a91ab74af69f0aee4
David Cardoner <cardoner1993@gmail.com>
2022-04-16 17:26:29 +0200

    Create binary-tree-postorder-traversal.scala

---
+++ tree/binary-tree-postorder-traversal.scala
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def postorderTraversal(root: TreeNode): List[Int] = {
+        
+        if (root == null) {
+            return List()
+        }
+
+        return postorderTraversal(root.left) ++ postorderTraversal(root.right) ++ List(root.value)
+    }
+}
